b993b6a0f085ac7a8a60c0f82b1e0fc43e6370a1
martinbalucha <martin.balucha@outlook.com>
2025-04-23 20:23:28 +0200

    Created pdf reader

---
+++ backend/src/EntityExtractor.Domain/PdfReading/PdfTextReader.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Text;
+using UglyToad.PdfPig;
+
+namespace eKultura.EntityExtractor.Domain.PdfReading;
+
+public class PdfTextReader
+{
+    private readonly ILogger<PdfTextReader> _logger;
+
+    public PdfTextReader(ILogger<PdfTextReader> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<string> ReadAsync(MemoryStream stream)
+    {
+        _logger.LogInformation("Attempting to open the memory stream for reading.");
+
+        using var pdf = PdfDocument.Open(stream);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        _logger.LogInformation("Starting reading process of the memory stream.");
+
+        foreach (var page in pdf.GetPages())
+        {
+            stringBuilder.Append(page.GetWords());
+        }
+
+        return Task.FromResult(stringBuilder.ToString());
+    }
+}
