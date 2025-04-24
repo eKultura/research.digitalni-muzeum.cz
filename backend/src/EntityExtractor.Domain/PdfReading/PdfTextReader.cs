a98effa5123168040bc68316a599f7485ef12ffa
martinbalucha <martin.balucha@outlook.com>
2025-04-24 20:32:49 +0200

    Added handling of nonpdf doc + unit test

--- backend/src/EntityExtractor.Domain/PdfReading/PdfTextReader.cs
+++ backend/src/EntityExtractor.Domain/PdfReading/PdfTextReader.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System.Text;
+using UglyToad.PdfPig.Core;
+using UglyToad.PdfPig.Exceptions;
 using PdfPigDocument = UglyToad.PdfPig.PdfDocument;
 
 namespace eKultura.EntityExtractor.Domain.PdfReading;
@@ -19,7 +21,7 @@ public class PdfTextReader
     {
         _logger.LogInformation("Attempting to open the memory stream for reading.");
 
-        using var pdf = PdfPigDocument.Open(stream);      
+        using var pdf = OpenDocument(stream);      
 
         _logger.LogInformation("Starting reading process of the memory stream.");
 
@@ -43,5 +45,21 @@ public class PdfTextReader
             wordCount, pageCount);
 
         return Task.FromResult(new PdfDocument(pageCount, wordCount, stringBuilder.ToString()));
+    }
+
+    private static PdfPigDocument OpenDocument(MemoryStream stream)
+    {
+        try
+        {
+            return PdfPigDocument.Open(stream);
+        }
+        catch (PdfDocumentFormatException ex)
+        {
+            throw new InvalidOperationException("Provided file stream is not a PDF document.", ex);
+        }
+        catch (PdfDocumentEncryptedException ex)
+        {
+            throw new InvalidOperationException("Cannot read the PDF file because it is encrypted.", ex);
+        }
     }
 }
---
+++ backend/src/EntityExtractor.Test/PdfReading/PdfTextReaderTest.cs
@@ -0,0 +1,30 @@
+﻿using eKultura.EntityExtractor.Domain.PdfReading;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Xml.Linq;
+
+namespace eKultura.EntityExtractor.Test.PdfReading;
+
+public class PdfTextReaderTest
+{
+    private readonly PdfTextReader _pdfReader = new(NullLogger<PdfTextReader>.Instance);
+
+    [Fact]
+    public async Task ReadNonPdfDocument_InvalidOperationExceptionThrown()
+    {
+        // Arrange        
+        using var memoryStream = new MemoryStream();
+
+        XDocument xmlDoc = new XDocument();
+
+        var root = new XElement(XName.Get("RootDocument"), "This is root");
+        xmlDoc.Add(root);
+
+        xmlDoc.Save(memoryStream);
+
+        memoryStream.Position = 0;
+
+        // Act & Assert
+        await Assert.ThrowsAsync<InvalidOperationException>(async () => await _pdfReader.ReadTextAsync(memoryStream));
+    }
+
+}
