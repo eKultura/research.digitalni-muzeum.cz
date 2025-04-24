540ad22f2ded853beb40ab45f953220be1ecf18a
laura.baluchova <laura.menova@gmail.com>
2025-04-24 15:32:22 +0200

    add dockerfile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+   },
+   server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+   },
 })
