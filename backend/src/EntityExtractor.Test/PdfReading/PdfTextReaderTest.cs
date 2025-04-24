49d85c754aacf1c83239338fcea4703586199625
martinbalucha <martin.balucha@outlook.com>
2025-04-24 20:37:49 +0200

    Added test for empty doc

--- backend/src/EntityExtractor.Test/PdfReading/PdfTextReaderTest.cs
+++ backend/src/EntityExtractor.Test/PdfReading/PdfTextReaderTest.cs
@@ -1,6 +1,7 @@
 ﻿using eKultura.EntityExtractor.Domain.PdfReading;
 using Microsoft.Extensions.Logging.Abstractions;
 using System.Xml.Linq;
+using UglyToad.PdfPig.Writer;
 
 namespace eKultura.EntityExtractor.Test.PdfReading;
 
@@ -27,4 +28,25 @@ public class PdfTextReaderTest
         await Assert.ThrowsAsync<InvalidOperationException>(async () => await _pdfReader.ReadTextAsync(memoryStream));
     }
 
+    [Fact]
+    public async Task ReadEmptyPdfDocument_EmptyResultReturned()
+    {
+        // Arrange        
+        using var memoryStream = new MemoryStream();
+
+        using PdfDocumentBuilder builder = new PdfDocumentBuilder();
+        byte[] pdfDoc = builder.Build();
+
+        await memoryStream.WriteAsync(pdfDoc);
+        memoryStream.Position = 0;
+
+        // Act
+        var result = await _pdfReader.ReadTextAsync(memoryStream);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(0, result.PageCount);
+        Assert.Equal(0, result.WordCount);
+        Assert.Empty(result.Text);
+    }
 }
