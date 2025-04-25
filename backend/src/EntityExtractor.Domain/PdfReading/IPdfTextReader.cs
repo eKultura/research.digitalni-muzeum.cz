26e1cffe0a67280e878aaf3c00b734ff8205572f
martinbalucha <38705514+martinbalucha@users.noreply.github.com>
2025-04-25 15:17:31 +0200

    8-Implement pdf to string conversion (#9)
    * Created pdf reader
    
    * Drafted PDF reader
    
    * Created test project
    
    * Renamed method
    
    * Drafted elaborate information extraction
    
    * Added handling of nonpdf doc + unit test
    
    * Added test for empty doc
    
    * Finished PDF reading draft
    
    * Ran dotnet format
    
    * 3-Mock the first endpoint (#6)
    
    * Created basic domain barebones
    
    * Added mock endpoints
    
    * Created mock endpoints
    
    * Added eKultura namespace root

---
+++ backend/src/EntityExtractor.Domain/PdfReading/IPdfTextReader.cs
@@ -0,0 +1,13 @@
+﻿namespace eKultura.EntityExtractor.Domain.PdfReading;
+
+public interface IPdfTextReader
+{
+    /// <summary>
+    /// Reads text from the PDF file.
+    /// </summary>
+    /// <param name="stream">Memory stream containing PDF file</param>
+    /// <returns>PDF file with its text content stored in string.</returns>
+    /// <exception cref="InvalidOperationException">If the memory stream does not contain PDF file</exception>
+    /// <exception cref="InvalidOperationException">If the PDF file is encrypted</exception>
+    Task<PdfDocument> ReadTextAsync(MemoryStream stream);
+}
