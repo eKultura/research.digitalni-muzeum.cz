db5fcc43db625491a07d68207ed3672f50821592
martinbalucha <martin.balucha@outlook.com>
2025-04-25 21:48:57 +0200

    Defined PDF page

---
+++ backend/src/EntityExtractor.Domain/PdfReading/PdfPage.cs
@@ -0,0 +1,22 @@
+﻿namespace eKultura.EntityExtractor.Domain.PdfReading;
+
+/// <summary>
+/// Represents a page in a PDF document
+/// </summary>
+/// <param name="Number">Number of the page</param>
+/// <param name="Words">A list of words on the page</param>
+/// <param name="NumberLabel">Number label on the page. Can be represented by a positive integer,
+/// but also by a roman number, e.g. in case of a book preface.</param>
+public record PdfPage(int Number, IList<string> Words, string? NumberLabel = null)
+{
+    private int _number;
+
+    public int Number
+    {
+        get => _number;
+        init => _number = value >= PdfReadingConstants.MinPdfPageNumber ? value 
+            : throw new ArgumentException("PDF page has to be a positive integer.");
+    }
+
+    public string VirtualNumber { get; init; } = NumberLabel ?? Number.ToString();
+}
--- backend/src/EntityExtractor.Domain/PdfReading/PdfReadingConstants.cs
+++ backend/src/EntityExtractor.Domain/PdfReading/PdfReadingConstants.cs
@@ -3,4 +3,10 @@
 public static class PdfReadingConstants
 {
     public const char SpaceDelimiter = ' ';
+    public const int MinPdfPageNumber = 1;
+
+    /// <summary>
+    /// Represents page labels element in a PDF document
+    /// </summary>
+    public const string PageLabelsElementName = "/PageLabels";
 }
