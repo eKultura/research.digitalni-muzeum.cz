7868c013ca0c466668463c2cf10cf592dc483668
laura.baluchova <laura.menova@gmail.com>
2025-04-30 22:22:27 +0200

    add navbar with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import FileUploader from "./Components/FileUploader";
+import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
-    <FileUploader />
+    <>    
+    <div className="flex flex-col gap-2 mt-20 items-center text-center">      
+      
+      <FileUploader />      
+    </div>
+    </>
   );
 }
