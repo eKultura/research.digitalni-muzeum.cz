5b02c6f2c15b55ab9be115afffef8a3f2130c28a
laurabaluchova <106871634+laurabaluchova@users.noreply.github.com>
2025-05-05 15:37:35 +0200

    add navbar (#29)
    * add navbar
    
    * adjust vite from 6.3.3 to 6.3.4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from "react-router";
+import DocumentFolders from './Components/DocumentFolders.jsx';
+import Layout from './Components/Layout.jsx';
+import SearchTool from './Components/SearchTool.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path="documents" element={<DocumentFolders />}/>
+        <Route path="searching" element={<SearchTool />}/>
+      </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
