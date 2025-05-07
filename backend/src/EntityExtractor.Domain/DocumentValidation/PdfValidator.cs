df00ff88076fa3e5b34e707773ca0199724acb94
martinbalucha <martin.balucha@outlook.com>
2025-05-07 21:39:55 +0200

    Added testing of extension

---
+++ backend/src/EntityExtractor.Domain/DocumentValidation/PdfValidator.cs
@@ -0,0 +1,38 @@
+﻿using eKultura.EntityExtractor.Contracts;
+
+namespace eKultura.EntityExtractor.Domain.DocumentValidation;
+
+public class PdfValidator
+{
+    private const string PdfExtension = ".pdf";
+
+    public bool IsValid(PdfDocument pdfDocument)
+    {
+        return IsDocumentPdf(pdfDocument);
+    }
+
+    private bool IsDocumentPdf(PdfDocument document)
+    {
+        bool hasPdfExtension = Path.GetExtension(document.Name).Equals(PdfExtension, StringComparison.OrdinalIgnoreCase);
+
+        return hasPdfExtension && HasPdfSignature(document.Document);
+    }
+
+
+
+    /// <summary>
+    /// Determines whether the file has the PDF signature.
+    /// </summary>
+    /// <param name="bytes">A file in binary format</param>
+    /// <returns>True if the file has the PDF signature. Otherwise false</returns>
+    /// <remarks>The PDF files have a file signature that starts with "%PDF-" (25 50 44 46 2D in hex)</remarks>
+    private bool HasPdfSignature(byte[] bytes)
+    {
+        return bytes.Length >= 5 &&
+               bytes[0] == 0x25 && // %
+               bytes[1] == 0x50 && // P
+               bytes[2] == 0x44 && // D
+               bytes[3] == 0x46 && // F
+               bytes[4] == 0x2D; // -
+    }
+}
---
+++ backend/src/EntityExtractor.Test/DocumentValidation/PdfValidatorTest.cs
@@ -0,0 +1,31 @@
+﻿using eKultura.EntityExtractor.Contracts;
+using eKultura.EntityExtractor.Domain.DocumentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eKultura.EntityExtractor.Test.DocumentValidation;
+
+public class PdfValidatorTest
+{
+    private readonly PdfValidator _pdfValidator = new();
+
+    [Fact]
+    public void IsValid_WrongExtension_ReturnsFalse()
+    {
+        // Arrange
+        var pdf = PdfTestUtils.BuildPdf(["Test pdf"]);
+
+        var pdfDocument = new PdfDocument("test.docx", pdf, "History");
+
+        // Act
+        bool isValid = _pdfValidator.IsValid(pdfDocument);
+
+        // Assert
+        Assert.False(isValid);
+    }
+
+
+}
