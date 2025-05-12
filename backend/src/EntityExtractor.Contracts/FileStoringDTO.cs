f9f35473edfd20a3f60d5aefc64737db0f378129
Jakub Duch <jakub.duch23@gmail.com>
2025-05-12 11:13:38 +0200

    Adding basic functionality

---
+++ backend/src/EntityExtractor.Contracts/FileStoringDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace eKultura.EntityExtractor.Contracts;
+
+public class FileStoringDTO
+{
+    public string Project { get; set; }
+    public byte[] PdfFile { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ backend/src/EntityExtractor.Domain/FileStoring/FileStoring.cs
@@ -0,0 +1,34 @@
+﻿using eKultura.EntityExtractor.Contracts;
+using System.IO.Abstractions;
+
+namespace eKultura.EntityExtractor.Domain.FileStoring;
+public class FileStoring : IFileStoring
+{
+    private readonly IFileSystem _fileSystem;
+    private readonly string _baseFolder;
+
+    public FileStoring(IFileSystem fileSystem, string baseFolder)
+    {
+        _fileSystem = fileSystem;
+        _baseFolder = baseFolder;
+    }
+    public FileStoringDTO StoreFile(string topic, byte[] pdfFile)
+    {
+        if (string.IsNullOrEmpty(topic))
+        {
+            throw new ArgumentException("Topic field is required.");
+        }
+
+        if(pdfFile == null)
+        {
+            throw new ArgumentException("Pdf file has to be selected before storing.");
+        }
+
+        return new FileStoringDTO
+        {
+            Project = topic,
+            PdfFile = pdfFile,
+            CreatedAt = DateTime.Now
+        };
+    }
+}
